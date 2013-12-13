485dc4d1e7c5263d3f0dcaf271cc91b558fda2ed
Younes Sherlock <younesagma@gmail.com>
2013-12-13 20:19:33 +0000

    Fixed a typo

--- test/custom_functions-test.js
+++ test/custom_functions-test.js
@@ -5,7 +5,7 @@ var custom = require('./../custom_functions.js');
 describe('All tests', function(){
 	describe('getCountry() Test', function(){
 		var value = "KADAMBI, Geeta, Riddhi IP LLC Patent Agent 43526 Gallegos Avenue Fremont, CA 94539 (US)";
-		var country = custom.getCountry();
+		var country = custom.getCountry(value);
 		expect(country).to.equal("US");
 	})
 });
