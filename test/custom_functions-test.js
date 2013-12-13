3afb23170070c00196d63d84bfa49938298fd502
Younes Sherlock <younesagma@gmail.com>
2013-12-13 22:00:02 +0000

    3 functions are now tested

--- test/custom_functions-test.js
+++ test/custom_functions-test.js
@@ -3,10 +3,30 @@ var custom = require('../custom_functions.js');
 
 
 describe('All tests', function(){
+
 	describe('getCountry() Test', function(){
 		var value = "KADAMBI, Geeta, Riddhi IP LLC Patent Agent 43526 Gallegos Avenue Fremont, CA 94539 (US)";
 		var country = custom.getCountry(value);
 		expect(country).to.equal("US");
-	})
+	});
+
+	describe('getFullName() Test', function(){
+		var value = "KADAMBI, Geeta, Riddhi IP LLC Patent Agent 43526 Gallegos Avenue Fremont, CA 94539 (US)";
+		var fullName = custom.getFullName(value);
+		expect(fullName).to.equal("KADAMBI, Geeta");
+	});
+
+	describe('getValueSingleChild() Test#1', function(){
+		var value = "METHOD, PROCESS AND SYSTEM TO ATOMICALLY STRUCTURE VARIED DATA AND TRANSFORM INTO CONTEXT ASSOCIATED DATA";
+		var result = custom.getValueSingleChild(value, "Title");
+		expect(result).to.equal(value);
+	});
+
+	describe('getValueSingleChild() Test#2', function(){
+		var value = "Oct 26, 2012";
+		var result = custom.getValueSingleChild(value, "FilingDate");
+		expect(country).to.equal("26/10/2012");
+	});
+	
 });
 
