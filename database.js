d5bf4b393873c659b4320be57e90e7817f071cc0
Younes Sherlock <younesagma@gmail.com>
2013-12-17 22:21:16 +0000

    Keyword Schema added

--- database.js
+++ database.js
@@ -5,6 +5,12 @@ var countrySchema = new mongoose.Schema({
     name: String
 });
 
+var keywordSchema = new mongoose.Schema({
+    patentId: { type: Number, ref: 'Patent' },
+    name: String,
+    // count: Number
+});
+
 var applicantSchema = new mongoose.Schema({
     _id: String,
     country: { type: String, ref: 'Country' }
@@ -15,31 +21,33 @@ var inventorSchema = new mongoose.Schema({
     country: { type: String, ref: 'Country' }
 });
 
-var patentSchema = mongoose.Schema({
+var patentSchema = new mongoose.Schema({
     _id: Number,
     title: String,
     abstractPatent: String,
     publicationDate: Date
 });
 
-var patentApplicantSchema = mongoose.Schema({
+var patentApplicantSchema = new mongoose.Schema({
     patentId: { type: Number, ref: 'Patent' },
     applicant: { type: String, ref: 'Applicant' },
     country: { type: String, ref: 'Country' },
     dateP: Date
 });
 
-var patentInventorSchema = mongoose.Schema({
+var patentInventorSchema = new mongoose.Schema({
     patentId: { type: Number, ref: 'Patent' },
     inventor: { type: String, ref: 'Inventor' },
     country: { type: String, ref: 'Country' },
     dateP: Date
 });
 
-mongoose.connect('mongodb://localhost/test_sid');
+mongoose.connect('mongodb://localhost/test');
+
 
 exports.mongoose = mongoose;
 exports.Country = mongoose.model('Country', countrySchema);
+exports.Keyword = mongoose.model('Keyword', keywordSchema);
 exports.Applicant = mongoose.model('Applicant', applicantSchema);
 exports.Inventor = mongoose.model('Inventor', inventorSchema);
 exports.Patent = mongoose.model('Patent', patentSchema);
